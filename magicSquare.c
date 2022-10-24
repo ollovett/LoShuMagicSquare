6efcbcfb4ee9c76724aa120e8321d9f98d8a22c0
Owen Lovett <ollovett@cpp.edu>
2022-10-23 17:10:16 -0700

    init outline

---
+++ magicSquare.c
@@ -0,0 +1,31 @@
+//magic square test function
+    //accept 2d array as argument
+    //add each element in all rows together and store in variables for each row
+    //do same as above but for columns
+    //do same for diaganals
+    //compare all variables
+        //if all are equal to each other then return true
+        //else return false
+
+//main
+    //call magic square test method with array that is a magic square
+    //call magic square test method with array that is not a magic square
+
+    //while x = 0 (not a magic square)
+        //initialize 1d array
+        //for loop
+            //do while generated number is not unique
+                //for loop
+                    //check if equal to any numbers in array
+                        //if equal than number is not unique
+
+            //store new variable
+
+        //copy prevois array into 2d array
+        //call magic square test function
+            //if returns true than x = 1 (end)
+            //if false then
+                //counter++ and loop
+
+    //print counter
+    //print square
